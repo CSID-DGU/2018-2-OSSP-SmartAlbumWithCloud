4a53066535a5664ee138b463e8bd2eec48871732
joohun484 <joohun484@naver.com>
2019-10-07 21:18:17 +0900

    New Activity Added for Our Project

--- app/src/main/java/com/zjianhao/ui/Photo_dlAty.java
+++ app/src/main/java/com/zjianhao/ui/Photo_dlAty.java
@@ -1,12 +1,21 @@
 package com.zjianhao.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.zjianhao.R;
+import com.zjianhao.bean.Photo;
+import com.zjianhao.view.TouchImageView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -14,7 +23,9 @@ import org.json.JSONObject;
 
 import java.text.DecimalFormat;
 
+import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * Created by 张建浩（Clarence) on 2016-6-26 19:41.
@@ -22,56 +33,60 @@ import butterknife.InjectView;
  * the author's github: https://github.com/zhangjianhao
  */
 public class Photo_dlAty extends AppCompatActivity {
-    @InjectView(R.id.main_toolbar)
-    Toolbar mainToolbar;
-    @InjectView(R.id.info_time_tv)
-    TextView infoTimeTv;
-    @InjectView(R.id.info_week)
-    TextView infoWeek;
-    @InjectView(R.id.info_filename)
-    TextView infoFilename;
-    @InjectView(R.id.info_size)
-    TextView infoSize;
-    @InjectView(R.id.info_filepath_tv)
-    TextView infoFilepathTv;
-    @InjectView(R.id.info_location_tv)
-    TextView infoLocationTv;
+    @InjectView(R.id.selected_photo)
+    TouchImageView selected_photo;
+    @InjectView(R.id.photo_name)
+    TextView photoName;
 
+    private Photo photo;
+    private ImageLoader imageloader;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dl);
+        ButterKnife.inject(this);
+        imageloader = ImageLoader.getInstance();
+        photo = getIntent().getParcelableExtra("photo");
+        imageloader.displayImage(photo.getImgUrl(), selected_photo);
+        photoName.setText(photo.getName());
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finish();
+            overridePendingTransition(0, R.anim.activity_exit_anim);
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+    @OnClick({R.id.photo_send, R.id.photo_back})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.photo_back:
+                Log.e("Going Back : ", "photo_deail_back");
+                finish();
+                overridePendingTransition(0, R.anim.activity_exit_anim);
+                break;
+            case R.id.photo_send:
+                Log.e("Sending : ", photo.getLocation());
+
+
+                break;
+            default:
+                Log.e("Something Wrong", "ERROR");
+
+        }
+    }
+    public void shareSingleImage() {
+        // Get uri from file
+        String url = photo.getImgUrl();
+        Uri imageUri  = Uri.parse(photo.getImgUrl());
+
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
+        shareIntent.setType("image/*");
+        startActivity(Intent.createChooser(shareIntent, "Share to"));
     }
 
-    public String getFileSize(long fileS) {
-        DecimalFormat df = new DecimalFormat("#.00");
-        String fileSizeString = "";
-        if (fileS < 1024) {
-            fileSizeString = df.format((double) fileS) + "B";
-        } else if (fileS < 1048576) {
-            fileSizeString = df.format((double) fileS / 1024) + "KB";
-        } else if (fileS < 1073741824) {
-            fileSizeString = df.format((double) fileS / 1048576) + "MB";
-        } else {
-            fileSizeString = df.format((double) fileS / 1073741824) + "GB";
-        }
-        return fileSizeString;
-    }
-
-    public String parseJson(String json){
-        try {
-            JSONObject object = new JSONObject(json);
-            JSONObject result = object.getJSONObject("result");
-            String formatAddr = result.getString("formatted_address");
-            JSONArray pois = result.getJSONArray("pois");
-            if (pois.length()>0){
-                return pois.getJSONObject(0).getString("addr");
-            }
-            return formatAddr;
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
 
 }
